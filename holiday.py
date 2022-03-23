8e9294e6b5e655100626f113b617e517cd3241a9
jason-lai <jason_lai@trendmicro.com>
2022-03-23 09:01:24 +0800

    feat: check if today is Merry Xmas

---
+++ holiday.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+
+def say_hello():
+    today = __get_today()
+    if today.strftime("%m-%d") == "12-25":
+        return "Merry Xmas"
+    return "Today is not Xmas"
+
+
+def __get_today():
+    return date.today()
